fac2b0d6dfdd8db04e7d9dcbe945123ac21a5ae3
patricia <patriciapavia236@gmail.com>
2021-11-07 23:30:57 +1100

    remove index.ss, and add .env to git Ignore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
