598097acef387a60d2a176720f2b9f89e25d9405
patricia <patriciapavia236@gmail.com>
2021-11-07 23:29:40 +1100

    bring fetchMoviesReducer to store

--- src/store.js
+++ src/store.js
@@ -1,13 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { fetchMovieReducer } from './reducers/fetchMovieReducers';
 
+const reducer = combineReducers({
+	movieList: fetchMovieReducer,
+});
 
-const reducer = combineReducers({})
+const middleware = [thunk];
 
+const store = createStore(
+	reducer,
+	composeWithDevTools(applyMiddleware(...middleware))
+);
 
-const middleware = [thunk]
-
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)))
-
-export default store
+export default store;
