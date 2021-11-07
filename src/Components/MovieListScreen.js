3e6269146cfa8bbdb040c31aa3c2036b6e72f05e
patricia <patriciapavia236@gmail.com>
2021-11-07 23:24:30 +1100

    MovieListScreen to display movie list

---
+++ src/Components/MovieListScreen.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import {
+	Paper,
+	Grid,
+	Card,
+	CardContent,
+	CardMedia,
+	Typography,
+} from '@mui/material';
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	color: theme.palette.text.secondary,
+}));
+const MovieListScreen = ({ movie }) => {
+	return (
+		<React.Fragment>
+			<Grid item xs={6} md={4} lg={3}>
+				<Item>
+					<Card sx={{ maxWidth: 345 }}>
+						<CardMedia
+							component='img'
+							height='200'
+							image={movie.Poster}
+							alt='green iguana'
+						/>
+						<CardContent>
+							<Typography gutterBottom variant='h5' component='div'>
+								{movie.Title}
+							</Typography>
+							<Typography variant='body2' color='text.secondary'>
+								{movie.Actors}
+							</Typography>
+							<Grid container spacing={4}>
+								<Grid item xs={6} md={6} lg={6}>
+									Cinemaworld
+								</Grid>
+								<Grid item xs={6} md={6} lg={6}>
+									${}
+								</Grid>
+							</Grid>
+							<Grid container spacing={4}>
+								<Grid item xs={6} md={6} lg={6}>
+									Filmworld
+								</Grid>
+								<Grid item xs={6} md={6} lg={6}>
+									${movie.Price}
+								</Grid>
+							</Grid>
+						</CardContent>
+					</Card>
+				</Item>
+			</Grid>
+		</React.Fragment>
+	);
+};
+
+export default MovieListScreen;
