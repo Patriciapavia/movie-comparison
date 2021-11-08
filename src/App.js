5ef45ae0c8ae9e1140c025c52f63b6c514a31b81
patricia <patriciapavia236@gmail.com>
2021-11-09 00:27:16 +1100

    add matterial ui core and style to start styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {Container, Box} from '@mui/material';
 import MovieScreen from './Components/MovieResultScreen';
 import PropTypes from 'prop-types';
+
+import { ThemeProvider } from '@material-ui/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const themeLight = createMuiTheme({
+	palette: {
+		background: {
+			default: '#e4f0e2',
+		},
+	},
+});
+
+const themeDark = createMuiTheme({
+	palette: {
+		background: {
+			default: '#06202A',
+		},
+	},
+});
 function Item(props) {
 	const { sx, ...other } = props;
 	return (
@@ -25,33 +44,27 @@ Item.propTypes = {
 };
 function App() {
 	return (
-		<Container>
-			<Box
-				sx={{
-					display: 'flex',
-					justifyContent: 'center',
-					p: 1,
-					m: 1,
-				}}
-			>
-				<Item>
-					<h1>Prince's theatre</h1>
-					<h5>Classic Movies At Home</h5>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor
-					</p>
-					<p>
-						incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-						veniam, quis nostrud
-					</p>
-					<p> exercitation</p>
-				</Item>
-			</Box>
-			<Router>
-				<Route path='/' component={MovieScreen} exact />
-			</Router>
-		</Container>
+		<MuiThemeProvider theme={themeDark ? themeDark : themeLight}>
+			<Container color='06202A'>
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						p: 1,
+						m: 1,
+					}}
+				>
+					<Item>
+						<h1>Prince's theatre</h1>
+						<h5>Classic Movies At Home</h5>
+						<p>Where you can search and compare a cheaper provider</p>
+					</Item>
+				</Box>
+				<Router>
+					<Route path='/' component={MovieScreen} exact />
+				</Router>
+			</Container>
+		</MuiThemeProvider>
 	);
 }
 
